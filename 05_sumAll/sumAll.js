fecf2656736208522b8be042bb76cd13f6888ac9
Brian Bud <hello@brianbud.com>
2023-10-15 16:05:23 -0400

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let min = Math.min(num1, num2);
+  let max = Math.max(num1, num2);
+  let sum = 0;
 
+  if (min < 0) {
+    return "ERROR";
+  } else if (min >= 0) {
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+
+    return sum;
+  }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
