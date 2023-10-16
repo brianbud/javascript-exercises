848ec9b74bfad4306160d8b54ffc91db48a843d4
Brian Bud <hello@brianbud.com>
2023-10-16 19:37:09 -0400

    convert temps

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fah) {
+  let result = (((fah - 32) * 5) / 9).toFixed(1);
+  return Number(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  let result = ((cel * 9) / 5 + 32).toFixed(1);
+  return Number(result);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
