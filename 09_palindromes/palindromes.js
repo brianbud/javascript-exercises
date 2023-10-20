813b7a6d68ba20933eaf2a4278a6b9342da43341
Brian Bud <hello@brianbud.com>
2023-10-19 21:28:47 -0400

    palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const strWithoutSymbols = str.replace(/[^a-zA-Z0-9]/g, "");
+  let arr = strWithoutSymbols.toLowerCase().split("");
+  let reverseStr = strWithoutSymbols.toLowerCase().split("").reverse();
 
+  let isPalindrome = true; // assume the string is a palindrome by default
+  for (let i = 0; i < arr.length; i++) {
+    if (reverseStr[i] !== arr[i]) {
+      // if there is a mismatch
+      isPalindrome = false;
+      break;
+    }
+  }
+  return isPalindrome;
 };
 
 // Do not edit below this line
