0541ce4ea5968c5ae1d9a2ab0acb75aae7a45018
Brian Bud <hello@brianbud.com>
2023-10-19 22:16:34 -0400

    fibanocci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
-};
+function fibonacci(num) {
+  num = parseInt(num);
+  //base case
+  if (num < 0) {
+    return "OOPS";
+  } else if (num <= 1) {
+    return num;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
